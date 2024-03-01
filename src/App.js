1f02e204b014ef8e1c9eed579c77da5ddc9013f6
jebomin <jebomin0515@naver.com>
2024-03-02 00:40:55 +0900

    #7.3

--- src/App.js
+++ src/App.js
@@ -2,73 +2,39 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState(null);
-  const [usdAmount, setUSDAmount] = useState(0);
-  const [bitcoinAmount, setBitcoinAmount] = useState(0);
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  const handleAmountChange = (event) => {
-    setUSDAmount(event.target.value);
-  };
-
-  const handleCoinChange = (event) => {
-    setSelectedCoin(event.target.value);
-  };
-
-  useEffect(() => {
-    if (selectedCoin && usdAmount > 0) {
-      const selectedCoinData = coins.find(
-        (coin) => coin.symbol === selectedCoin
-      );
-      if (selectedCoinData) {
-        const bitcoinPrice = selectedCoinData.quotes.USD.price;
-        const bitcoinAmount = usdAmount / bitcoinPrice;
-        setBitcoinAmount(bitcoinAmount.toFixed(8)); // Limit to 8 decimal places
-      }
-    }
-  }, [selectedCoin, usdAmount, coins]);
-
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      <br />
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <label htmlFor="usdAmount">Enter USD Amount:</label>
-          <input
-            type="number"
-            id="usdAmount"
-            value={usdAmount}
-            onChange={handleAmountChange}
-          />
-          <br />
-          <label htmlFor="coinSelect">Select a Coin:</label>
-          <select id="coinSelect" onChange={handleCoinChange}>
-            <option value="">Select a coin</option>
-            {coins.map((coin) => (
-              <option key={coin.id} value={coin.symbol}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <br />
-          {selectedCoin && usdAmount > 0 && (
-            <p>
-              With ${usdAmount} USD, you can buy approximately {bitcoinAmount}{" "}
-              {selectedCoin}.
-            </p>
-          )}
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                <li>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </li>
+              </ul>
+            </div>
+          ))}
         </div>
       )}
     </div>
