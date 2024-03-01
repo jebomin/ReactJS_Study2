14c571b81d3d280818864bb04f33bedebdd3c8a8
jebomin <jebomin0515@naver.com>
2024-03-02 01:13:20 +0900

    #7.4

---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function Movie({ converImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={converImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        <li>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  converImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default Movie;
